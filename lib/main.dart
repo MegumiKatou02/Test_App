13746f1c1363b60d8a3f4e77d8498b73d49c0283
khongbachofficial <khongbachofficial@gmail.com>
2024-11-26 15:54:00 +0000

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:myapp/data/repository/repository.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:myapp/ui/home/home.dart';
 
-void main() async {
-  var repository = DefaultRepository();
-  var songs = await repository.loadData();
-  if (songs != null) {
-    for (var song in songs) {
-      debugPrint(song.toString());
-    }
-  }
-}
-
-class MusicApp extends StatelessWidget {
-  const MusicApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+void main() => runApp(const MyApp());
